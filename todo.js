0ceeab1eea584d8fd21a65c7d52fc63eb8c6a09e
stroopwafel79 <stroopwafel79@gmail.com>
2019-01-05 00:37:41 +0000

    Added toggleCompleted method

--- todo.js
+++ todo.js
@@ -159,6 +159,12 @@ const todoList = {
     this.todos[index].todoText = todoText;
     this.displayTodos();
   },
+
+  toggleCompleted: function(position) {
+    const todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
   deleteTodo: function(index) {
     this.todos.splice(index, 1);
     this.displayTodos();
