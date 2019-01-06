d1d926b12525fcbd02ca6f54e0901ad0a65bef55
stroopwafel79 <stroopwafel79@gmail.com>
2019-01-06 23:49:21 +0000

    Changed .displayTodos to indicate if a todo item in completed

--- todo.js
+++ todo.js
@@ -212,12 +212,17 @@ const todoList = {
 
 
   displayTodos: function() { // anonymous funct b/c access via todoList.displayTodos
-    if (this.todo.length === 0) {
+    if (this.todos.length === 0) {
       console.log('Your todo list is empty');
     } else {
       console.log('My todos:');
       for (let i = 0, i < this.todos.length, i++) {
-        console.log(this.todos[i].todoText);
+
+        if (this.todos[i].completed === true) {
+          console.log('[x]', this.todos[i].todoText);
+        } else {
+          console.log('[ ]', this.todos[i].todoText);
+        }
       }
     }
   },
